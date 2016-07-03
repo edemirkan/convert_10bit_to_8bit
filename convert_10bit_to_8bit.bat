42ff8546b6c7b2521d49cc16593fd90631921037
Evren Demirkan <evren.demirk4n@gmail.com>
2016-07-03 10:07:38 +0300

    Updated batch file with comments

--- convert_10bit_to_8bit.bat
+++ convert_10bit_to_8bit.bat
@@ -1,8 +1,8 @@
 :: Convert 10 bit video to 8 bit
 :: 05/06/2016 evren demirkan
 @echo off
-net use K: \\192.168.0.7\media\seagate_2.0tb 
-set "eight_bit_path=K:\anime\series.10bit_to_8bit_converted\"
+::net use K: \\network-location\share REM If your content is on a network share.. 
+set "eight_bit_path=C:\Destination\path\to\your\8bit video\"
 set anime_full_path=%~dp1
 set temp=%anime_full_path:~0,-1%
 for %%f in ("%temp%") do set anime_name=%%~nxf
@@ -10,4 +10,4 @@ set "new_path=%eight_bit_path%%anime_name%"
 md "%new_path%"
 
 %~dp0ffmpeg.exe -y -i %1 -map 0 -c copy -c:v nvenc_h264 -preset medium -threads 4 -stats -loglevel error -pix_fmt nv12 "%new_path%\%~n1.8bit%~x1"
-net use /delete K:
+::net use /delete K: REM Disconnect share after conversion
